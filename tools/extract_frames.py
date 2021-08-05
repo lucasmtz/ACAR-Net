c55aee29deef78975472a0f853a79e81b0bd29e9
lucasmtz <lucasmtz@gmail.com>
2021-08-05 12:57:50 +0200

    Improve script to extract frames from videos

--- tools/extract_frames.py
+++ tools/extract_frames.py
@@ -1,20 +1,23 @@
 import argparse
 import math
 import multiprocessing
+import shutil
 import os
 
 parser = argparse.ArgumentParser(description="Frame Extraction")
 parser.add_argument("--video_dir", type=str, required=True)
-parser.add_argument("--frame_dir", type=str, required=True)
-parser.add_argument("--num_processes", type=int, default=64)
+parser.add_argument("--num_processes", type=int, default=0)
 args = parser.parse_args()
-
+frame_dir = os.path.join(os.path.dirname(args.video_dir), 'frames')
 
 def main(video_file, frame_rate=30, clip_start=900, clip_end=1800):
     video_path = os.path.join(args.video_dir, video_file)
-
     video_id = video_file.rsplit(".", 1)[0]
-    frame_root = os.path.join(args.frame_dir, video_id)
+    frame_root = os.path.join(frame_dir, video_id)
+
+    if os.path.isdir(frame_root) and len(os.listdir(frame_root)) != 27001:
+        shutil.rmtree(frame_root)
+
     os.makedirs(frame_root, exist_ok=True)
 
     """EXTRACT FRAMES WITH FFMPEG"""
@@ -36,5 +39,9 @@ def main(video_file, frame_rate=30, clip_start=900, clip_end=1800):
 if __name__ == "__main__":
     videos = os.listdir(args.video_dir)
 
-    with multiprocessing.Pool(args.num_processes) as p:
+    if not args.num_processes:
+        num_processes = multiprocessing.cpu_count()
+    else: 
+        num_processes = args.num_processes
+    with multiprocessing.Pool(num_processes) as p:
         p.map(main, videos)
